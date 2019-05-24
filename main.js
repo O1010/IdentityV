343d3dedc9418d126aebda56994854f0f6d9f2c4
太陽光線 <o1010@taiyoukousennoMacBook-Pro.local>
2019-05-24 18:08:43 +0900

    修正

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function sidebar(rootDir){
     $.ajax({
     url: rootDir + "sidebar.html",
     cache: false,
+    async: false,
+    dataType: 'html',
     success: function(html){
     html = html.replace(/\{\$root\}/g, rootDir); //sidebar.htmlの{$root}を置換
     document.write(html);
