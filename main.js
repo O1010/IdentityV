89d248f8d10a119c52d60d137a33f1f4b331cd50
太陽光線 <o1010@taiyoukousennoMacBook-Pro.local>
2019-05-24 17:46:19 +0900

    js周り

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-$(function(){
-    $("#sidebar").load("../sidebar.html");
-  });
+function sidebar(rootDir){
+    $.ajax({
+    url: rootDir + "sidebar.html",
+    cache: false,
+    success: function(html){
+    html = html.replace(/\{\$root\}/g, rootDir); //sidebar.htmlの{$root}を置換
+    document.write(html);
+    }
+    });
+    }
