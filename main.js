2f93d7e283b71545798500b6f248bd3465d583a6
太陽光線 <o1010@taiyoukousennoMacBook-Pro.local>
2019-05-26 01:51:05 +0900

    最低限の形

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function sidebar(rootDir){
+function header(rootDir){
     $.ajax({
-    url: rootDir + "sidebar.html",
+    url: rootDir + "include/header.html",
     cache: false,
     async: false,
     dataType: 'html',
@@ -9,4 +9,17 @@ function sidebar(rootDir){
     document.write(html);
     }
     });
+}
+
+function sidebar(rootDir){
+    $.ajax({
+    url: rootDir + "include/sidebar.html",
+    cache: false,
+    async: false,
+    dataType: 'html',
+    success: function(html){
+    html = html.replace(/\{\$root\}/g, rootDir); //sidebar.htmlの{$root}を置換
+    document.write(html);
     }
+    });
+}
